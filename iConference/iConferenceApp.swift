40ef0a2b10ed32ad317c86685f5e3ee2d9f6c719
Eric Liu <ericliu4943@gmail.com>
2020-11-28 20:38:39 -0800

    Initial Commit

---
+++ iConference/iConferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConferenceApp.swift
+//  iConference
+//
+//  Created by Eric Liu on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct iConferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
